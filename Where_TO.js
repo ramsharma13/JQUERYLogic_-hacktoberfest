08d05ac1f6e73e1f72b816396b8669f2acac5554
Ravi Ranjan <90889370+Ravi4419@users.noreply.github.com>
2023-10-21 14:24:29 +0530

    Create Where_TO.js

---
+++ Where_TO.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Demo JavaScript in Body</h2>
+
+<p id="demo">A Paragraph.</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hactoberfest 2023";
+}
+</script>
+
+</body>
+</html> 
